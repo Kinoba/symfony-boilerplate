2baf0389bdab6e4f08932c7fbbb4c9f0165b9ae4
GCalmels <gabriel@kinoba.fr>
2019-10-14 08:53:42 +0200

    Add editorconfig and eslint

--- assets/js/components/Example.vue
+++ assets/js/components/Example.vue
@@ -1,13 +1,13 @@
 <template>
-   <div>
-       <p>This is an example of a new components in VueJs</p>
-   </div>
+  <div>
+    <p>This is an example of a new components in VueJs</p>
+  </div>
 </template>
 
 <script>
-   export default {
-       name: "example"
-   }
+export default {
+  name: 'Example'
+};
 </script>
 
 <style scoped>
